9d9276169027dad0b7560e185bb1e7281e9e6888
masawa200ok <project.masawa@gmail.com>
2015-09-15 01:02:53 +0900

    add tweets

--- inserter.rb
+++ inserter.rb
@@ -1,13 +1,3 @@
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-  adapter: 'sqlite3',
-  database: './db.sqlite3'
-)
-
-class Tweet < ActiveRecord::Base
-end
-
 open(ARGV[0]).each do |line|
   line.chomp!
   next if nil
@@ -15,16 +5,10 @@ open(ARGV[0]).each do |line|
     next
   end
   if line.size >= 140
-    puts "[#{line.size}] #{line}"
+    puts "[#{line.size}] [Bot]#{line}"
     next
-  end
-  t = Tweet.create(
-    tweet: line,
-    delete_flg: 'f'
-  )
-  unless t
-    p t
-    break
+  else
+    puts "[Bot]#{line}"
   end
 end
 
