27348d73b8f9a24c59707edcfff047da5ddcc23f
masawa200ok <project.masawa@gmail.com>
2015-09-15 10:11:28 +0900

    add program onerror

--- masawa200ok_bot.rb
+++ masawa200ok_bot.rb
@@ -11,12 +11,18 @@ open("#{current_dir}/tweets.db.txt") do |file|
 end
 tweet = array.sample
 
-client = Twitter::REST::Client.new do |config|
-  settings = YAML.load_file("#{current_dir}/settings.yaml")
-  config.consumer_key         = settings[:consumer_key]
-  config.consumer_secret      = settings[:consumer_key_secret]
-  config.access_token         = settings[:access_token]
-  config.access_token_secret  = settings[:access_token_secret]
+begin
+  client = Twitter::REST::Client.new do |config|
+    settings = YAML.load_file("#{current_dir}/settings.yaml")
+    config.consumer_key         = settings[:consumer_key]
+    config.consumer_secret      = settings[:consumer_key_secret]
+    config.access_token         = settings[:access_token]
+    config.access_token_secret  = settings[:access_token_secret]
+  end
+
+  client.update(tweet)
+  puts "#{Time.now}: #{tweet}"
+rescue => e
+  STDERR.puts "#{Time.now}: #{e}"
+  raise e
 end
-puts "#{Time.now}: #{tweet}"
-client.update(tweet)
